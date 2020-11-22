c5497f6caef237a6fa9fe77f6f1edccb52c5b8cd
luboslavistenik <luboslav.istenik@gmail.com>
2020-11-22 22:04:27 +0100

    Hero a Item

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,8 @@
+import { Item } from './item';
+
 export interface Hero {
     id: number;
     name: string;
+    itemList: Item[];
   }
+
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice',itemList:[{id: 11, name: 'Warmog', price: 2850 }]},
+  { id: 12, name: 'Narco', itemList:[{id: 12, name: 'Triforce',price: 3300}]},
+  { id: 13, name: 'Bombasto', itemList:[{id: 13, name: 'Rabaddon', price: 3600}]},
+  { id: 14, name: 'Celeritas', itemList:[{id: 14, name: 'Hurricane', price: 2600}]},
+  { id: 15, name: 'Magneta', itemList:[{id: 15, name: 'Void Staff', price: 2650}]},
+  { id: 16, name: 'RubberMan', itemList:[{id: 16, name: 'IE', price: 3400, },{id: 17, name: 'Rageblade', price: 3100}]},
+  { id: 17, name: 'Dynama', itemList:[{id: 17, name: 'Rageblade', price: 3100}]},
+  { id: 18, name: 'Dr IQ', itemList:[{id: 18, name: 'PD', price: 2600}]},
+  { id: 19, name: 'Magma', itemList:[{id: 19, name: 'LDR', price: 2800}]},
+  { id: 20, name: 'Tornado', itemList:[{id: 20, name: 'BT', price: 3500}]}
 ];
