00a208c8e0b8ecd3b686353c48fc80c3ed311696
luboslavistenik <luboslav.istenik@gmail.com>
2020-12-14 13:00:04 +0100

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ItemsComponent } from './items/items.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { ItemsComponent } from './items/items.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    ItemsComponent
+    ItemsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
